ae26aeb11b3bf667c7c4da1ed70a0c3300519b18
TrashPanda94 <31078941+TrashPanda94@users.noreply.github.com>
2019-10-22 14:54:14 -0700

    Update tests.hs

--- tests.hs
+++ tests.hs
@@ -1,4 +1,3 @@
--- Vector2 Test
 import System.IO
 import Control.Monad (replicateM)
 --import System.Random (randomRIO, StdGen, randomR, mkStdGen)
@@ -62,3 +61,68 @@ half x = x `divToFloat` 2
 
 center :: (Integral a) => a -> a -> Vector2
 center width height = Vector2 (half width) (half height)
+-- *Main> center 4 4
+-- Vector2 2.0 2.0
+
+unitCircle :: Float -> Vector2
+unitCircle angle = Vector2 (cos angle) (sin angle)
+-- *Main> unitCircle 0.5
+-- Vector2 0.87758255 0.47942555
+-- *Main> unitCircle 0
+-- Vector2 1.0 0.0
+-- *Main> unitCircle (pi/6)
+-- Vector2 0.8660254 0.5
+-- *Main> (sqrt 3)/2
+-- 0.8660254037844386
+
+
+polygonPoints :: Int -> [Vector2]
+polygonPoints num_sides = [unitCircle (theta (fromIntegral i)) | i <- [1..num_sides]]
+  where
+    theta :: Float -> Float
+    theta index = index * 2 * pi / (fromIntegral num_sides)
+-- Main> polygonPoints 3
+-- [Vector2 (-0.50000006) 0.8660254,Vector2 (-0.4999999) (-0.86602545),Vector2 1.0 1.7484555e-7]
+-- points at theta -pi/3, -2pi/3, 0
+
+svgPoints :: [Vector2] -> Attribute
+svgPoints (h:t) = Attribute "points" (showPoint h ++ showPoints t)
+  where
+    showPoint (Vector2 x y) = show x ++ "," ++ show y
+    showPoints [] = ""
+    showPoints (h:t) =  " " ++ showPoint h ++ showPoints t
+
+
+polyline :: [Vector2] -> Node
+polyline points = Node "polyline" [svgPoints points] []
+
+polygon :: [Vector2] -> Node
+polygon points = Node "polygon" [svgPoints points] []
+
+
+path :: [Vector2] -> Node
+path points = Node "path" [(Attribute "d" (d "M" points))] []
+  where
+    d command [] = " Z"
+    d command ((Vector2 x y):rest) = command ++ show x ++ " " ++ show y ++ d " L" rest
+
+line :: Vector2 -> Vector2 -> Node
+line (Vector2 x1 y1) (Vector2 x2 y2) =
+  Node "line" [attr "x1" x1, attr "y1" y1, attr "x2" x2, attr "y2" y2] []
+
+
+buildLines :: [Vector2] -> [Node]
+buildLines points = [line one two | (one, two) <- zip points (tail points)]
+
+
+spurs :: Vector2 -> Vector2 -> Node
+spurs origin (Vector2 offsetX offsetY) = polyline points
+  where
+    endpoint x = offset origin (Vector2 (offsetX * x) offsetY)
+    points = [endpoint (-1), origin, endpoint 1]
+
+rotate :: (Show a, Num a) => a -> Vector2 -> Attribute
+rotate deg (Vector2 centerX centerY) =
+  Attribute "transform" ("rotate(" ++ show deg ++ "," ++ show centerX ++ "," ++ show centerY ++ ")")
+
+
